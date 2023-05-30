9609bfcf923a3d15846ad7ccb36873c3a386918c
mizukami-hash <128657673+mizukami-hash@users.noreply.github.com>
2023-05-30 19:38:59 +0900

    Add files via upload

---
+++ TODOリスト(2)/main.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const table = document.querySelector("table");
+const priority = document.querySelector("select"); 
+const deadline = document.querySelector('input[type="date"]'); 
+
+document.querySelector('submit').addEventListener('click',()=>{
+
+  const item ={};
+  const todo = document.querySelector("#todo"); 
+  item.todo=todo.value;
+  
+  const tr =document.querySelector('tr');
+  
+
+
+});
