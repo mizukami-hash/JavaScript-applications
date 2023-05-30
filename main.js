546b15d2eae93464d8d1fc3145bb969bb43f321d
mizukami-hash <128657673+mizukami-hash@users.noreply.github.com>
2023-05-30 19:49:15 +0900

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-const taskValue =document.getElementByClassName('task_value')[0];
-const taskSubmit=document.getElementByClassName('task_submit')[0];
-const taskList =document.getElementByClassName('task_list')[0];
-
-const addTasks=(task)=>{
-  const listItem =dosument.createElement('li');
-}
